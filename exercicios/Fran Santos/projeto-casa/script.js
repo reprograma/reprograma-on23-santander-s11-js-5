aed18a5c303d5c7ae6b66e05e50af15938b3b45f
FrancieleCSantos <francielecoceicaodossantos93@gmail.com>
2023-06-08 21:23:32 -0300

    Desafio conclúido, aleluia..

--- exercicios/Fran Santos/projeto-casa/script.js
+++ exercicios/Fran Santos/projeto-casa/script.js
@@ -1,54 +1,64 @@
-function buscarDigi() {
+function pesquisarDigi() {
     const digimom = document.getElementById('BuscarDigimon').value
     const url = ` https://digimon-api.vercel.app/api/digimon/name/${digimom}`;
 
 
-    fetch(url).then(function(response){
+    fetch(url).then(function (response) {
 
         response.json().then(mostarDigi)
     })
-    }
+}
 
-    function mostarDigi(dados) {
-        console.log("digimom:"+JSON.stringify(dados))
+function mostarDigi(dados) {
+    console.log("digimom:" + JSON.stringify(dados))
     const NomeDoDigimon = document.getElementById('NomeDoDigimon');
     NomeDoDigimon.innerHTML = dados[0].name
     const nivelDoDigimon = document.getElementById('nivelDoDigimon');
     nivelDoDigimon.innerHTML = dados[0].level
-    const imgDigi= document.getElementById('imgDigi');
+    const imgDigi = document.getElementById('imgDigi');
     imgDigi.src = dados[0].img
 
-    }
-
-
-    const container = document.getElementById('listaDeDigimon');
-
-function CriarCards(meusDigis) {
-    return `
-     <section class="cards">
-    //   <img class="imagens" src=${meusDigis[0].img}>
-      <h2 class="titles">${meusDigis[0].name}</h2>
-     </section>
-    `
-
 }
 
-async function getDigis() {
+const ul = document.getElementById('renderizarListaDeDigimon')
+async function getDigimons() {
     try {
-        const responde = await fetch('https://digimon-api.vercel.app/api/digimon');
-        const filmesFavoritos = await responde.json()
-        const meusFilmesFavoritos = filmesFavoritos.slice(0,30);
-        return meusFilmesFavoritos
+        const response = await fetch('https://digimon-api.vercel.app/api/digimon')
+        const json = await response.json()
+        return json
     }
     catch (erro) {
-        console.error("Ops, algo deu errado: ", + erro);
+        console.log("capturei um erro: " + erro)
     }
 }
 
-async function renderizarNaTela() {
-    const filmesFavoritos = await getDigis()
-    filmesFavoritos.map((meusDigis) => container.innerHTML += CriarCards(meusDigis))
+async function RenderizarDigimon() {
+    try {
+        const digimons = await getDigimons()
+        digimons.forEach(digis => {
+            ul.innerHTML += createHTML(digis)
+        });
+    }
+    catch (erro) {
+        console.log("capturei um erro: " + erro)
+    }
 }
 
-renderizarNaTela()
+function createHTML(digis) {
+    return `
+        <button onclick="detalharDigimon('${digis.name}')">${digis.name}</button> `
+}
 
+async function detalharDigimon(detalhe) {
+    try {
+        const url = `https://digimon-api.vercel.app/api/digimon/name/${detalhe}`;
+        const response = await fetch(url)
+        const digimon = await response.json()
+        resultado(digimon)
+    }
+    catch (erro) {
+        console.log("capturei um erro: " + erro)
+    }
+}
+
+RenderizarDigimon()
