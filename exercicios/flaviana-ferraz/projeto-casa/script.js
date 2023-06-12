1e6631acc2161a1e0429c41a8bc76b636c0bf8b3
Flaviana <flaviferraz@yahoo.com.br>
2023-06-12 20:01:37 -0300

    funcionalidade do botão com o nome do digimon, aparecimento do card do digimon clicado, css, responsividade

--- exercicios/flaviana-ferraz/projeto-casa/script.js
+++ exercicios/flaviana-ferraz/projeto-casa/script.js
@@ -1,82 +1,50 @@
-const container = document.getElementById('demo')
-// const buscar = document.getElementById('buscar')
+const digimonsList = document.getElementById("lista-digimons");
+const card = document.getElementById("card");
 
+// const buscar = document.getElementById('buscar')
 
 // buscar.addEventListener('click', (event)=> {
 //     event.preventDefault()
 //     findDigimon(digimon)
 // })
 
-
-// digimon.addEventListener('click', (event)=>{
-//     event.preventDefault();
-//     createCardDigimon('${digi.name}');
-// });
-
-
-async function getDigimons(){
-    try {
-        const digimons = await fetch(`https://digimon-api.vercel.app/api/digimon/`) 
-        const digimonNames = await digimons.json()
-        return digimonNames
-    } catch (erro) {
-        console.error("Erro capturado: " + erro)
-    }
+async function getDigimons() {
+  try {
+    const digimons = await fetch("https://digimon-api.vercel.app/api/digimon/");
+    const digimonNames = await digimons.json();
+    renderDigimons(digimonNames);
+  } catch (erro) {
+    console.error("Erro capturado: " + erro);
+  }
 }
 
-function input(digimon){
-    return `
-    <button id="digi" onclick="createCardDigimon('${digimon.name}')">${digimon.name}</button>
-  `
+function renderDigimons(digimons) {
+  digimons.forEach(
+    (digimon) =>
+      digimonsList.innerHTML += 
+      `<input id="digi" type="submit" value="${digimon.name}" 
+      onclick="getOneDigimon(this.value)">`
+  )
 }
 
-async function main(){
-    try {
-        const digiName = await getDigimons( )
-        digiName.map((digimon) => {container.innerHTML += input(digimon)})  
-    } catch (erro) {
-        console.error("Erro capturado: " + erro)
-    }     
+async function getOneDigimon(nome) {
+  try {
+    const response = await fetch(
+      `https://digimon-api.vercel.app/api/digimon/name/${nome}`
+    );
+    const digimon = await response.json();
+    createCardDigimon(digimon[0]);
+  } catch (erro) {
+    console.error("Erro capturado: " + erro);
+  }
 }
 
-function createCardDigimon(digi){
-    const img = document.getElementById('imgDigimon')
-    img.src = digi[0].img
-
-    const digimon = document.getElementById('digimonName')
-    digimon.innerHTML = digi[0].name
-
-    const level = document.getElementById('level')
-    level.innerHTML = digi[0].level
+function createCardDigimon(digimon) {
+  card.innerHTML = `
+    <img src= ${digimon.img}>
+    <h3> ${digimon.name} </h3>
+    <p> Level: ${digimon.level} </p>
+    `;
 }
 
-async function getOneDigimon(digi){
-    try {
-        let digimon = await fetch(`https://digimon-api.vercel.app/api/digimon/name/${digi}`);
-        let infos = await digimon.json();
-        createCardDigimon(digi)
-    } catch (erro) {
-        console.error("Erro capturado: " + erro)  
-    }
-}
-
-main() 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+getDigimons();
